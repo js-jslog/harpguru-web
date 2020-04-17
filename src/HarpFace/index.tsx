bd45b49c23d01fa9a13db17087eec5a8402fd35a
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-17 16:08:19 +0000

    Achieve iterative harpface build with a 1d array

--- src/HarpFace/index.tsx
+++ src/HarpFace/index.tsx
@@ -11,6 +11,13 @@ const harpfaceValues = [
   [ '13', '14', '15', '16' ]
 ]
 
+const harpfaceValuesFlat = [
+  undefined, '2', '3', '4',
+  '5', '6', '7', '8',
+  '9', '10', '11', '12',
+  '13', '14', '15', '16'
+]
+
 export class HarpFace extends React.Component {
   renderSquare(x: number, y: number): ReactElement {
     return <Square value={harpfaceValues[x][y]}/>
@@ -18,30 +25,9 @@ export class HarpFace extends React.Component {
   render(): ReactElement {
     return (
       <div className='harpface'>
-        <div className='harprow'>
-          { this.renderSquare(0,0) }
-          { this.renderSquare(0,1) }
-          { this.renderSquare(0,2) }
-          { this.renderSquare(0,3) }
-        </div>
-        <div className='harprow'>
-          { this.renderSquare(1,0) }
-          { this.renderSquare(1,1) }
-          { this.renderSquare(1,2) }
-          { this.renderSquare(1,3) }
-        </div>
-        <div className='harprow'>
-          { this.renderSquare(2,0) }
-          { this.renderSquare(2,1) }
-          { this.renderSquare(2,2) }
-          { this.renderSquare(2,3) }
-        </div>
-        <div className='harprow'>
-          { this.renderSquare(3,0) }
-          { this.renderSquare(3,1) }
-          { this.renderSquare(3,2) }
-          { this.renderSquare(3,3) }
-        </div>
+        {harpfaceValuesFlat.map(function (value, index) {
+          return <Square key={index} value={value} />
+        })}
       </div>
     )
   }
