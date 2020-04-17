768984ecd4650916bbd7cc7f4497c71d58c33440
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-17 15:43:52 +0000

    Test the flexibility of the css grid layout for empty cells

--- src/HarpFace/Square.tsx
+++ src/HarpFace/Square.tsx
@@ -3,14 +3,20 @@ import type { ReactElement } from 'react'
 import './Square.css'
 
 interface SquareInputProps {
-  value: string;
+  value: string | undefined;
 }
 
 export class Square extends React.Component<SquareInputProps> {
   render(): ReactElement {
+    const { props: { value } } = this
+    // The css grid layout being used at the moment requires
+    // a non-null text value for the cell to be displayed
+    const contents = value || '/'
+    let className = 'square'
+    if (value === undefined) className += ' no-contents'
     return (
-      <div className="square">
-        {this.props.value}
+      <div className={className}>
+        { contents }
       </div>
     )
   }
--- src/HarpFace/index.tsx
+++ src/HarpFace/index.tsx
@@ -5,7 +5,7 @@ import { Square } from './Square'
 import './HarpFace.css'
 
 const harpfaceValues = [
-  [ '1', '2', '3', '4' ],
+  [ undefined, '2', '3', '4' ],
   [ '5', '6', '7', '8' ],
   [ '9', '10', '11', '12' ],
   [ '13', '14', '15', '16' ]
