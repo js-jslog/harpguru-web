26a7bbcf6484f9bb94102ad039d1cc918aac32d7
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-17 16:36:01 +0000

    Achieve iterative rendering of input 2d array

--- src/HarpFace/index.tsx
+++ src/HarpFace/index.tsx
@@ -11,23 +11,22 @@ const harpfaceValues = [
   [ '13', '14', '15', '16' ]
 ]
 
-const harpfaceValuesFlat = [
-  undefined, '2', '3', '4',
-  '5', '6', '7', '8',
-  '9', '10', '11', '12',
-  '13', '14', '15', '16'
-]
+type harpValue = string | undefined
 
 export class HarpFace extends React.Component {
-  renderSquare(x: number, y: number): ReactElement {
-    return <Square value={harpfaceValues[x][y]}/>
+  generateSquares = (twoD: harpValue[][]): ReactElement[] => {
+    const mapped = twoD.map(function (row: harpValue[], indexy) {
+      return row.map(function (value: harpValue, indexx) {
+        return <Square key={indexx + indexy} value={value} />
+      })
+    })
+    const flattened = mapped.flat()
+    return flattened
   }
   render(): ReactElement {
     return (
       <div className='harpface'>
-        {harpfaceValuesFlat.map(function (value, index) {
-          return <Square key={index} value={value} />
-        })}
+        { this.generateSquares(harpfaceValues) }
       </div>
     )
   }
