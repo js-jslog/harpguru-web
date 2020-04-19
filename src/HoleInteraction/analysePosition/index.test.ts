6c72e88fcff3ae35d62ba75ce61d08aaa0099779
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-19 18:12:54 +0000

    Add function for HoleInteraction to anaylse it's position

---
+++ src/HoleInteraction/analysePosition/index.test.ts
@@ -0,0 +1,5 @@
+import { analysePosition } from './index'
+
+test('analysePosition is a function', () => {
+  expect(analysePosition).toBeDefined()
+})
---
+++ src/HoleInteraction/analysePosition/index.ts
@@ -0,0 +1,1 @@
+export const analysePosition = (): boolean => true
