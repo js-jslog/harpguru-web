2a54d97bbd8885b38d5f7444295d4af4b0da62df
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-24 22:17:53 +0000

    Remove commented out redundant code

--- src/HarpFace/getBlowDrawBoundaryIndex/index.test.tsx
+++ src/HarpFace/getBlowDrawBoundaryIndex/index.test.tsx
@@ -1,7 +1,7 @@
 import { InteractionIds } from 'harpstrata'
-import type { InteractionMatrix, InteractionRow } from 'harpstrata'
+import type { InteractionMatrix } from 'harpstrata'
 
-import { getBlowDrawBoundaryIndex, getBlowDrawBoundaryIndexRow } from './index'
+import { getBlowDrawBoundaryIndex } from './index'
 
 test('getBlowDrawBoundaryIndex returns the correct boundary index from an InteractionMatrix', () => {
   const interactionMatrix: InteractionMatrix = [
@@ -13,15 +13,3 @@ test('getBlowDrawBoundaryIndex returns the correct boundary index from an Intera
   const actualIndex = getBlowDrawBoundaryIndex(interactionMatrix)
   expect(actualIndex).toBe(expectedIndex)
 })
-
-//test('getBlowDrawBoundaryIndexRow returns the correct boundary index from an InteractionRow', () => {
-//  const interactionRow: InteractionRow = [
-//    {id: InteractionIds.Blow}, {id: InteractionIds.Blow},
-//    {id: InteractionIds.Draw}, {id: InteractionIds.Draw},
-//    {id: InteractionIds.Bend1}, {id: InteractionIds.Bend1},
-//  ]
-//  const expectedIndex = 2
-//  const indexTuple: [number, boolean] = [0, false]
-//  const actualIndex = getBlowDrawBoundaryIndexRow(indexTuple, interactionRow)
-//  expect(actualIndex[0]).toBe(expectedIndex)
-//})
