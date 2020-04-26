927081c0597c54b2af9a3465f1ac206b7472ec72
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-26 19:10:40 +0000

    Simplify test

--- src/HarpFace/HoleInteractionRows/index.test.tsx
+++ src/HarpFace/HoleInteractionRows/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { getApparatusIds, getPozitionIds, getHarpStrata, InteractionIds } from 'harpstrata'
+import { getApparatusIds, getPozitionIds, getHarpStrata, InteractionMatrix, InteractionIds } from 'harpstrata'
 import { render } from '@testing-library/react'
 
 import type { HarpFaceProps } from '../types'
@@ -15,12 +15,7 @@ const harpFaceProps: HarpFaceProps = {
   theme,
 }
 
-test('HoleInteractionRowsTop returns an array, the end of which is a Blow', () => {
-  const downToBlow = HoleInteractionRowsTop(harpFaceProps)
-  const lastBlowHole = downToBlow.slice(-1)
-  const { getByText: getByTextLastBlowHole } = render(<div>{lastBlowHole}</div>)
-
-  const { harpstrata: { apparatus: { interactionMatrix }, degreeMatrix}} = harpFaceProps
+const getBlowInteractionRowIndex = (interactionMatrix: InteractionMatrix): number => {
   // This reducer assumes that there is a Blow row somewhere in the matrix
   // If there isn't then this isn't going to function
   const blowInteractionRow = interactionMatrix.reduce((blowRow, thisRow) => {
@@ -31,7 +26,19 @@ test('HoleInteractionRowsTop returns an array, the end of which is a Blow', () =
     if (interactionId === InteractionIds.Blow) return thisRow
     return blowRow
   })
-  const blowInteractionRowIndex = interactionMatrix.indexOf(blowInteractionRow)
+
+  return interactionMatrix.indexOf(blowInteractionRow)
+}
+
+const { harpstrata: { apparatus: { interactionMatrix }, degreeMatrix}} = harpFaceProps
+const blowInteractionRowIndex = getBlowInteractionRowIndex(interactionMatrix)
+
+
+test('HoleInteractionRowsTop returns an array, the end of which is a Blow', () => {
+  const downToBlow = HoleInteractionRowsTop(harpFaceProps)
+  const lastBlowHole = downToBlow.slice(-1)
+  const { getByText: getByTextLastBlowHole } = render(<div>{lastBlowHole}</div>)
+
   const { [blowInteractionRowIndex]: blowDegreeRow } = degreeMatrix
   const [ lastBlow ] = blowDegreeRow.slice(-1)
   const { id: lastBlowId } = lastBlow || {id: 'fail'}
@@ -43,19 +50,6 @@ test('HoleInteractionRowsBottom returns an array, the start of which is a Draw',
   const downFromDraw = HoleInteractionRowsBottom(harpFaceProps)
   const [ firstDrawHole ] = downFromDraw
   const { getByText: getByTextFirstDrawHole } = render(<div>{firstDrawHole}</div>)
-
-  const { harpstrata: { apparatus: { interactionMatrix }, degreeMatrix}} = harpFaceProps
-  // This reducer assumes that there is a Blow row somewhere in the matrix
-  // If there isn't then this isn't going to function
-  const blowInteractionRow = interactionMatrix.reduce((blowRow, thisRow) => {
-    const [ interaction ] = thisRow
-    if (interaction === undefined) return blowRow
-
-    const { id: interactionId } = interaction
-    if (interactionId === InteractionIds.Blow) return thisRow
-    return blowRow
-  })
-  const blowInteractionRowIndex = interactionMatrix.indexOf(blowInteractionRow)
 
   const { [blowInteractionRowIndex +1]: drawDegreeRow } = degreeMatrix
   const [ firstDraw ] = drawDegreeRow
