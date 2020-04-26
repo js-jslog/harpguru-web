72b47b7a8e65454c9a643227a5f1d6cf1870b546
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-26 20:57:48 +0000

    Make example test resource more consistent
    It's better to be specific because the order of the apparatus and
    pozitions presented from the getters might change. If that happens then
    the test object would change and presumably negatively impact the tests.

--- src/HarpFace/testResources/index.ts
+++ src/HarpFace/testResources/index.ts
@@ -1,10 +1,10 @@
-import { getApparatusIds, getPozitionIds, getHarpStrata } from 'harpstrata'
+import { ApparatusIds, PozitionIds, getHarpStrata } from 'harpstrata'
 
 import type { HarpFaceProps } from '../index'
 import { getTheme } from '../../Theme'
 
 
-const harpStrata = getHarpStrata(getApparatusIds()[0], getPozitionIds()[0])
+const harpStrata = getHarpStrata(ApparatusIds.MajorDiatonic, PozitionIds.First)
 const theme = getTheme()
 
 export const exampleHarpFaceProps: HarpFaceProps = {
