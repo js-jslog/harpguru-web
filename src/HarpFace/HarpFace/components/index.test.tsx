5598ab7a71a6623847b45ca32f6c19ff3c627b8d
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-27 20:11:39 +0000

    Use common example test data

--- src/HarpFace/HarpFace/components/index.test.tsx
+++ src/HarpFace/HarpFace/components/index.test.tsx
@@ -1,34 +1,15 @@
 import React from 'react'
-import type { HarpStrata } from 'harpstrata'
-import { ApparatusIds, DegreeIds, InteractionIds } from 'harpstrata'
 import { render } from '@testing-library/react'
 
-import { getTheme } from '../../index'
+import { exampleHarpFaceProps } from '../../testResources'
 
 import { HarpFace } from './index'
-
-const apparatus = {
-  id: ApparatusIds.MajorDiatonic,
-  halfstepIndexMatrix: [[0, 1], [undefined, 2]],
-  interactionMatrix: [
-    [{id: InteractionIds.Blow}, {id: InteractionIds.Blow}],
-    [undefined                , {id: InteractionIds.Draw}],
-  ],
-}
-const harpStrata: HarpStrata = {
-  apparatus,
-  degreeMatrix: [
-    [{ id: DegreeIds.Root }, { id: DegreeIds.Second }],
-    [undefined             , { id: DegreeIds.Third }],
-  ]
-}
-const theme = getTheme()
 
 test('HarpFace renders a dom element with the required grid styles when a two column strata matrices are provided', () => {
   const expectedStyle = `
     display: grid;
-    grid-template-columns: auto auto;
+    grid-template-columns: auto auto auto auto auto auto auto auto auto auto;
   `
-  const { container } = render(<HarpFace harpStrata={ harpStrata } theme={theme} />)
+  const { container } = render(<HarpFace {...exampleHarpFaceProps} />)
   expect(container.firstChild).toHaveStyle(expectedStyle)
 })
