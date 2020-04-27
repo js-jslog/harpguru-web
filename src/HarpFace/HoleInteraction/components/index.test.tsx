9014bc5bdccf5cf21833cf01bbfa6fd590521265
js-jslog <jhs4jbs@hotmail.co.uk>
2020-04-27 20:18:05 +0000

    Use common example test data

--- src/HarpFace/HoleInteraction/components/index.test.tsx
+++ src/HarpFace/HoleInteraction/components/index.test.tsx
@@ -1,32 +1,13 @@
 import React from 'react'
-import { ApparatusIds, InteractionIds } from 'harpstrata'
-import type { HarpStrata } from 'harpstrata'
 import { DegreeIds } from 'harpstrata'
 import { render } from '@testing-library/react'
 
-import { getTheme } from '../../index'
+import { exampleHarpFaceProps } from '../../testResources'
 
 import { HoleInteraction } from './index'
 
-const apparatus = {
-  id: ApparatusIds.MajorDiatonic,
-  halfstepIndexMatrix: [[ 0, 1]],
-  interactionMatrix: [
-    [{id: InteractionIds.Blow}, {id: InteractionIds.Blow}],
-    [undefined                , {id: InteractionIds.Draw}],
-  ],
-}
-const harpStrata: HarpStrata = {
-  apparatus,
-  degreeMatrix: [
-    [{ id: DegreeIds.Root }, { id: DegreeIds.Second }],
-    [undefined             , { id: DegreeIds.Third }],
-  ]
-}
-const theme = getTheme()
-
 test('HoleInteraction renders a dom element with the expected value included', () => {
-  const { getByText } = render(<HoleInteraction harpStrata={harpStrata} yxCoord={[0,1]} theme={theme} />)
+  const { getByText } = render(<HoleInteraction {...exampleHarpFaceProps} yxCoord={[3,0]} />)
   expect(getByText(DegreeIds.Second)).toBeInTheDocument()
 })
 
@@ -34,11 +15,11 @@ test('HoleInteraction renders an invisible dom element if it is undefined', () =
   const expectedStyle = `
     visibility: hidden;
   `
-  const { container } = render(<HoleInteraction harpStrata={harpStrata} yxCoord={ [1,0] } theme={theme} />)
+  const { container } = render(<HoleInteraction {...exampleHarpFaceProps} yxCoord={ [0,0] } />)
   expect(container.firstChild).toHaveStyle(expectedStyle)
 })
 
 test('HoleInteraction renders with a class identifying it\'s position in the matrix', () => {
-  const { getByText } = render(<HoleInteraction harpStrata={harpStrata} yxCoord={ [0,0] } theme={theme} />)
+  const { getByText } = render(<HoleInteraction {...exampleHarpFaceProps} yxCoord={ [0,0] } />)
   expect(getByText(DegreeIds.Root).getAttribute('class')).toMatch(/yx-coord-0-0/)
 })
