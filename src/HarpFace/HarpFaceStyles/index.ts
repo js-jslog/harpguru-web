81fa5749f95ad04065b9fe42d04ddd5fc5da53c4
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-01 10:55:32 +0000

    Add a style generator function for HoleInteraction

--- src/HarpFace/HarpFaceStyles/index.ts
+++ src/HarpFace/HarpFaceStyles/index.ts
@@ -1,24 +1,7 @@
-import { InteractionIds } from 'harpstrata'
+import { generateHoleInteractionStyles } from '../HoleInteraction'
 
 import type { HarpFaceStyles } from './types'
 
-const BLOW_COLOR_PRIMARY = '#FFEFE5' as const
-const BLOW_COLOR_SECONDARY = '#FFCB7C' as const
-const DRAW_COLOR_PRIMARY = '#D5E1F2' as const
-const DRAW_COLOR_SECONDARY = '#3C67A7' as const
+const holeInteractionStyles = generateHoleInteractionStyles()
 
-const HOLE_INTERACTION_STYLES = {
-  [ InteractionIds.Blow ]: { color: 'black', backgroundColor: BLOW_COLOR_PRIMARY },
-  [ InteractionIds.Draw ]: { color: 'black', backgroundColor: DRAW_COLOR_PRIMARY },
-  [ InteractionIds.Bend1 ]: { color: 'white', backgroundColor: DRAW_COLOR_SECONDARY },
-  [ InteractionIds.Bend2 ]: { color: 'white', backgroundColor: DRAW_COLOR_SECONDARY },
-  [ InteractionIds.Bend3 ]: { color: 'white', backgroundColor: DRAW_COLOR_SECONDARY },
-  [ InteractionIds.BlowBend1 ]: { color: 'white', backgroundColor: BLOW_COLOR_SECONDARY },
-  [ InteractionIds.BlowBend2 ]: { color: 'white', backgroundColor: BLOW_COLOR_SECONDARY },
-  [ InteractionIds.OverDraw1 ]: { color: 'white', backgroundColor: DRAW_COLOR_SECONDARY },
-  [ InteractionIds.OverBlow1 ]: { color: 'white', backgroundColor: BLOW_COLOR_SECONDARY },
-} as const
-
-export const generateStyles = (): HarpFaceStyles => ({
-  holeInteractionStyles: HOLE_INTERACTION_STYLES,
-})
+export const generateStyles = (): HarpFaceStyles => ({holeInteractionStyles})
--- src/HarpFace/HoleInteraction/index.ts
+++ src/HarpFace/HoleInteraction/index.ts
@@ -1,3 +1,4 @@
 export { HoleInteraction } from './components'
 export type { HoleInteractionProps } from './types'
+export { generateHoleInteractionStyles } from './styles'
 export type { HoleInteractionStyles } from './styles'
