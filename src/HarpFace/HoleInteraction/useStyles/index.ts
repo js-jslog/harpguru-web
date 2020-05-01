ffda6f5a1a1a66a3ae1d221285dae2947a31704c
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-01 08:28:06 +0000

    Add a box shadow for active holes

--- src/HarpFace/HoleInteraction/useStyles/index.ts
+++ src/HarpFace/HoleInteraction/useStyles/index.ts
@@ -5,6 +5,7 @@ import type { UseStylesProps } from '../types'
 type Styles = {
   readonly borderRadius: string;
   readonly backgroundColor: string;
+  readonly boxShadow: string;
   readonly color: string;
   readonly fontSize: string;
   readonly height: string;
@@ -22,6 +23,7 @@ const determineStyles = (props: UseStylesProps): Styles => {
 
   const borderRadius = `${sizesMap[4]}px`
   const backgroundColor = (isActive ? themedBackgroundColor : 'transparent')
+  const boxShadow = (isActive ? `0 ${sizesMap[3]}px ${sizesMap[3]}px #ddd` : '0')
   const color = (isActive ? themedColor : 'black')
   const fontSize = `${sizesMap[5]}px`
   const height = `${sizesMap[6]}px`
@@ -30,13 +32,14 @@ const determineStyles = (props: UseStylesProps): Styles => {
   const visibility = (thisDegree === undefined ? 'hidden' : 'visible')
   const width = `${sizesMap[6]}px`
 
-  return { borderRadius, backgroundColor, color, fontSize, height, marginTop, marginBottom, visibility, width }
+  return { borderRadius, backgroundColor, boxShadow, color, fontSize, height, marginTop, marginBottom, visibility, width }
 }
 
 export const useStyles = createUseStyles({
   holeInteractionClass: {
     'border-radius': (props: UseStylesProps): string => determineStyles(props).borderRadius,
     'border-width': '0px',
+    'box-shadow': (props: UseStylesProps): string => determineStyles(props).boxShadow,
     'background-color': (props: UseStylesProps): string => determineStyles(props).backgroundColor,
     color: (props: UseStylesProps): string => determineStyles(props).color,
     'font-size': (props: UseStylesProps): string => determineStyles(props).fontSize,
