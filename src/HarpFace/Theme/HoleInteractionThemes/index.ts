d49080edec387b1089358b6df3dbcf8e7ffc7654
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-01 09:31:03 +0000

    Move holeInteractionTheme to a dedicated module within Theme

---
+++ src/HarpFace/Theme/HoleInteractionThemes/index.ts
@@ -0,0 +1,18 @@
+import { InteractionIds } from 'harpstrata'
+
+const BLOW_COLOR_PRIMARY = '#FFEFE5' as const
+const BLOW_COLOR_SECONDARY = '#FFCB7C' as const
+const DRAW_COLOR_PRIMARY = '#D5E1F2' as const
+const DRAW_COLOR_SECONDARY = '#3C67A7' as const
+
+export const HOLE_INTERACTION_THEMES = {
+  [ InteractionIds.Blow ]: { color: 'black', backgroundColor: BLOW_COLOR_PRIMARY },
+  [ InteractionIds.Draw ]: { color: 'black', backgroundColor: DRAW_COLOR_PRIMARY },
+  [ InteractionIds.Bend1 ]: { color: 'white', backgroundColor: DRAW_COLOR_SECONDARY },
+  [ InteractionIds.Bend2 ]: { color: 'white', backgroundColor: DRAW_COLOR_SECONDARY },
+  [ InteractionIds.Bend3 ]: { color: 'white', backgroundColor: DRAW_COLOR_SECONDARY },
+  [ InteractionIds.BlowBend1 ]: { color: 'white', backgroundColor: BLOW_COLOR_SECONDARY },
+  [ InteractionIds.BlowBend2 ]: { color: 'white', backgroundColor: BLOW_COLOR_SECONDARY },
+  [ InteractionIds.OverDraw1 ]: { color: 'white', backgroundColor: DRAW_COLOR_SECONDARY },
+  [ InteractionIds.OverBlow1 ]: { color: 'white', backgroundColor: BLOW_COLOR_SECONDARY },
+} as const
--- src/HarpFace/Theme/index.ts
+++ src/HarpFace/Theme/index.ts
@@ -1,12 +1,8 @@
-import { InteractionIds } from 'harpstrata'
+import { HOLE_INTERACTION_THEMES } from './HoleInteractionThemes'
 
 import type { Theme, ThemeProps, SizesMap } from './types'
 
 const defaultThemeParams = { seedSize: 1 } as const
-const BLOW_COLOR_PRIMARY = '#FFEFE5' as const
-const BLOW_COLOR_SECONDARY = '#FFCB7C' as const
-const DRAW_COLOR_PRIMARY = '#D5E1F2' as const
-const DRAW_COLOR_SECONDARY = '#3C67A7' as const
 
 const getSizesMap = (themeProps: ThemeProps): SizesMap => {
   const { seedSize } = themeProps
@@ -23,16 +19,6 @@ const getSizesMap = (themeProps: ThemeProps): SizesMap => {
 
 export const getTheme = (themeProps: ThemeProps = defaultThemeParams): Theme => ({
   sizesMap: getSizesMap(themeProps),
-  holeInteractionThemes: {
-    [ InteractionIds.Blow ]: { color: 'black', backgroundColor: BLOW_COLOR_PRIMARY },
-    [ InteractionIds.Draw ]: { color: 'black', backgroundColor: DRAW_COLOR_PRIMARY },
-    [ InteractionIds.Bend1 ]: { color: 'white', backgroundColor: DRAW_COLOR_SECONDARY },
-    [ InteractionIds.Bend2 ]: { color: 'white', backgroundColor: DRAW_COLOR_SECONDARY },
-    [ InteractionIds.Bend3 ]: { color: 'white', backgroundColor: DRAW_COLOR_SECONDARY },
-    [ InteractionIds.BlowBend1 ]: { color: 'white', backgroundColor: BLOW_COLOR_SECONDARY },
-    [ InteractionIds.BlowBend2 ]: { color: 'white', backgroundColor: BLOW_COLOR_SECONDARY },
-    [ InteractionIds.OverDraw1 ]: { color: 'white', backgroundColor: DRAW_COLOR_SECONDARY },
-    [ InteractionIds.OverBlow1 ]: { color: 'white', backgroundColor: BLOW_COLOR_SECONDARY },
-  },
+  holeInteractionThemes: HOLE_INTERACTION_THEMES,
 })
 export type { Theme } from './types'
