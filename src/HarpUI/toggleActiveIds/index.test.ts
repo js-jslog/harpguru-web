0b9c099623c83cceebaad7d570947760dd0b0e30
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-09 22:34:38 +0000

    Improve toggle function to remove element too

--- src/HarpUI/toggleActiveIds/index.test.ts
+++ src/HarpUI/toggleActiveIds/index.test.ts
@@ -7,7 +7,18 @@ test('toggleActiveDegreeIds will take a DegreeId and add it to an ActiveDegreeId
   const activeDegreeIds: ActiveDegreeIds = []
   const { Root: degreeId } = DegreeIds
   const setActiveDegreeIds = jest.fn()
-  const expectedActiveDegreeIds = [ DegreeIds.Root ]
+  const expectedActiveDegreeIds: ActiveDegreeIds = [ DegreeIds.Root ]
+
+  toggleActiveDegreeIds(degreeId, activeDegreeIds, setActiveDegreeIds)
+
+  expect(setActiveDegreeIds.mock.calls[0][0]).toEqual(expectedActiveDegreeIds)
+})
+
+test('toggleActiveDegreeIds will take a DegreeId and remove it from an ActiveDegreeIds which does already have the Id in it and calls a setter function with the result', () => {
+  const activeDegreeIds: ActiveDegreeIds = [ DegreeIds.Root ]
+  const { Root: degreeId } = DegreeIds
+  const setActiveDegreeIds = jest.fn()
+  const expectedActiveDegreeIds: ActiveDegreeIds = []
 
   toggleActiveDegreeIds(degreeId, activeDegreeIds, setActiveDegreeIds)
 
--- src/HarpUI/toggleActiveIds/index.ts
+++ src/HarpUI/toggleActiveIds/index.ts
@@ -1,5 +1,11 @@
 import type { DegreeIds, ActiveDegreeIds } from 'harpstrata'
 
 export const toggleActiveDegreeIds = (degreeId: DegreeIds, activeDegreeIds: ActiveDegreeIds, setActiveDegreeIds: Function): void => {
-  setActiveDegreeIds([ ...activeDegreeIds, degreeId ])
+  if (activeDegreeIds.includes(degreeId)) {
+    const arrayHead = activeDegreeIds.slice(0, activeDegreeIds.indexOf(degreeId))
+    const arrayTail = activeDegreeIds.slice(activeDegreeIds.indexOf(degreeId) +1)
+    setActiveDegreeIds([ ...arrayHead, ...arrayTail ])
+  } else {
+    setActiveDegreeIds([ ...activeDegreeIds, degreeId ])
+  }
 }
