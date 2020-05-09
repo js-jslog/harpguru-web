4f8693a5bc8b2e5e8b5d2de3afc8cda16ce82b01
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-09 22:38:26 +0000

    Shore up the naive toggle function with more tests

--- src/HarpUI/toggleActiveIds/index.test.ts
+++ src/HarpUI/toggleActiveIds/index.test.ts
@@ -14,11 +14,44 @@ test('toggleActiveDegreeIds will take a DegreeId and add it to an ActiveDegreeId
   expect(setActiveDegreeIds.mock.calls[0][0]).toEqual(expectedActiveDegreeIds)
 })
 
-test('toggleActiveDegreeIds will take a DegreeId and remove it from an ActiveDegreeIds which does already have the Id in it and calls a setter function with the result', () => {
+test('toggleActiveDegreeIds will take a DegreeId and remove it from an ActiveDegreeIds which only has that Id in it and calls a setter function with the result', () => {
   const activeDegreeIds: ActiveDegreeIds = [ DegreeIds.Root ]
   const { Root: degreeId } = DegreeIds
   const setActiveDegreeIds = jest.fn()
   const expectedActiveDegreeIds: ActiveDegreeIds = []
+
+  toggleActiveDegreeIds(degreeId, activeDegreeIds, setActiveDegreeIds)
+
+  expect(setActiveDegreeIds.mock.calls[0][0]).toEqual(expectedActiveDegreeIds)
+})
+
+test('toggleActiveDegreeIds will take a DegreeId and remove it from the end of an ActiveDegreeIds which does already have the Id in it and calls a setter function with the result', () => {
+  const activeDegreeIds: ActiveDegreeIds = [ DegreeIds.Second, DegreeIds.Root ]
+  const { Root: degreeId } = DegreeIds
+  const setActiveDegreeIds = jest.fn()
+  const expectedActiveDegreeIds: ActiveDegreeIds = [ DegreeIds.Second ]
+
+  toggleActiveDegreeIds(degreeId, activeDegreeIds, setActiveDegreeIds)
+
+  expect(setActiveDegreeIds.mock.calls[0][0]).toEqual(expectedActiveDegreeIds)
+})
+
+test('toggleActiveDegreeIds will take a DegreeId and remove it from the start of an ActiveDegreeIds which does already have the Id in it and calls a setter function with the result', () => {
+  const activeDegreeIds: ActiveDegreeIds = [ DegreeIds.Root, DegreeIds.Second ]
+  const { Root: degreeId } = DegreeIds
+  const setActiveDegreeIds = jest.fn()
+  const expectedActiveDegreeIds: ActiveDegreeIds = [ DegreeIds.Second ]
+
+  toggleActiveDegreeIds(degreeId, activeDegreeIds, setActiveDegreeIds)
+
+  expect(setActiveDegreeIds.mock.calls[0][0]).toEqual(expectedActiveDegreeIds)
+})
+
+test('toggleActiveDegreeIds will take a DegreeId and remove it from the middle of an ActiveDegreeIds which does already have the Id in it and calls a setter function with the result', () => {
+  const activeDegreeIds: ActiveDegreeIds = [ DegreeIds.Third, DegreeIds.Fourth, DegreeIds.Root, DegreeIds.Second ]
+  const { Root: degreeId } = DegreeIds
+  const setActiveDegreeIds = jest.fn()
+  const expectedActiveDegreeIds: ActiveDegreeIds = [ DegreeIds.Third, DegreeIds.Fourth, DegreeIds.Second ]
 
   toggleActiveDegreeIds(degreeId, activeDegreeIds, setActiveDegreeIds)
 
