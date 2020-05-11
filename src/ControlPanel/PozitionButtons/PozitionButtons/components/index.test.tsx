df14dc50a71573426487520d6578bb68f59d17bf
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-11 16:43:56 +0000

    Add first button in to PozitionButtons wrapper component

--- src/ControlPanel/PozitionButtons/PozitionButtons/components/index.test.tsx
+++ src/ControlPanel/PozitionButtons/PozitionButtons/components/index.test.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
+
+import type { PozitionButtonsProps } from '../types'
 
 import { PozitionButtons } from './index'
 
 test('PozitionButtons renders a dom element with a \'Harp Position\' label', () => {
-  const { getByText } = render(<PozitionButtons />)
+  const setPozitionId = jest.fn()
+  const pozitionButtonsProps: PozitionButtonsProps = { setPozitionId }
+
+  const { getByText } = render(<PozitionButtons {...pozitionButtonsProps } />)
   expect(getByText('Harp Position')).toBeInTheDocument()
+})
+
+test('PozitionButtons renders a first position button which calls the passed function parameter when clicked', () => {
+  const setPozitionId = jest.fn()
+  const pozitionButtonsProps: PozitionButtonsProps = { setPozitionId }
+
+  const { getByText } = render(<PozitionButtons {...pozitionButtonsProps } />)
+
+  expect(getByText('First')).toBeInTheDocument()
+  fireEvent.click(getByText('First'))
+  expect(setPozitionId.mock.calls.length).toBe(1)
 })
--- src/ControlPanel/PozitionButtons/PozitionButtons/components/index.tsx
+++ src/ControlPanel/PozitionButtons/PozitionButtons/components/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import type { ReactElement } from 'react'
+import { PozitionIds } from 'harpstrata'
 
-export function PozitionButtons(): ReactElement {
+import type { PozitionButtonsProps } from '../types'
+import { PozitionButton } from '../../PozitionButton'
+
+export function PozitionButtons(props: PozitionButtonsProps): ReactElement {
+  const { First: id } = PozitionIds
+  const label = 'First'
+  const { setPozitionId } = props
+  const firstPozitionButtonProps = { id, label, setPozitionId }
+
   return (
-    <div>Harp Position</div>
+    <div>
+      <div>Harp Position</div>
+      <PozitionButton {...firstPozitionButtonProps} />
+    </div>
   )
 }
