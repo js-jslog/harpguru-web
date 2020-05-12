8571fd56403be73c71193a5495cd4a1f5895b8dd
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-12 06:48:47 +0000

    Add test expectation to PitchButton

--- src/ControlPanel/PitchButtons/PitchButton/components/index.test.tsx
+++ src/ControlPanel/PitchButtons/PitchButton/components/index.test.tsx
@@ -9,16 +9,17 @@ import { PitchButton } from './index'
 test('PitchButton renders a component with the id used as the label', () => {
   const { A: id } = PitchIds
   const setPitchId = jest.fn()
-  const pozitionButtonProps: PitchButtonProps = { id, setPitchId }
-  const { getByText } = render(<PitchButton {...pozitionButtonProps} />)
+  const pitchButtonProps: PitchButtonProps = { id, setPitchId }
+  const { getByText } = render(<PitchButton {...pitchButtonProps} />)
   expect(getByText(id)).toBeInTheDocument()
 })
 
-test('PitchButton renders a component which calls a paramaterised function when clicked', () => {
+test('PitchButton renders a component which calls a paramaterised function when clicked, passing the id as the argument to that function call', () => {
   const { A: id } = PitchIds
   const setPitchId = jest.fn()
-  const pozitionButtonProps: PitchButtonProps = { id, setPitchId }
-  const { getByText } = render(<PitchButton {...pozitionButtonProps} />)
+  const pitchButtonProps: PitchButtonProps = { id, setPitchId }
+  const { getByText } = render(<PitchButton {...pitchButtonProps} />)
   fireEvent.click(getByText(id))
   expect(setPitchId.mock.calls.length).toBe(1)
+  expect(setPitchId.mock.calls[0][0]).toBe(id)
 })
