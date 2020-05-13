c662b117be0c85e16d91221249b3f39815c2253e
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-13 15:18:05 +0000

    Correct variable name

--- src/ControlPanel/ApparatusButtons/ApparatusButton/components/index.test.tsx
+++ src/ControlPanel/ApparatusButtons/ApparatusButton/components/index.test.tsx
@@ -9,16 +9,16 @@ import { ApparatusButton } from './index'
 test('ApparatusButton renders a component with the id used as the label', () => {
   const { MajorDiatonic: id } = ApparatusIds
   const setApparatusId = jest.fn()
-  const pozitionButtonProps: ApparatusButtonProps = { id, setApparatusId }
-  const { getByText } = render(<ApparatusButton {...pozitionButtonProps} />)
+  const apparatusButtonProps: ApparatusButtonProps = { id, setApparatusId }
+  const { getByText } = render(<ApparatusButton {...apparatusButtonProps} />)
   expect(getByText(id)).toBeInTheDocument()
 })
 
 test('ApparatusButton renders a component which calls a paramaterised function when clicked', () => {
   const { MajorDiatonic: id } = ApparatusIds
   const setApparatusId = jest.fn()
-  const pozitionButtonProps: ApparatusButtonProps = { id, setApparatusId }
-  const { getByText } = render(<ApparatusButton {...pozitionButtonProps} />)
+  const apparatusButtonProps: ApparatusButtonProps = { id, setApparatusId }
+  const { getByText } = render(<ApparatusButton {...apparatusButtonProps} />)
   fireEvent.click(getByText(id))
   expect(setApparatusId.mock.calls.length).toBe(1)
 })
