789f30c1472350aa4e45ec6376d9baf99d193edd
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-13 15:48:56 +0000

    Improve ControlPanel tests readibility and coverage
    I still don't know whether its best practice to have such similar tests
    all the way down the call chain from control panel to control button,
    but I do suspect it's a good idea to have fully penatrative tests at the
    top and when your components lower down are so basic, there's going to
    be some overlap.

--- src/ControlPanel/ControlPanel/components/index.test.tsx
+++ src/ControlPanel/ControlPanel/components/index.test.tsx
@@ -8,7 +8,7 @@ import { DisplayModes } from '../../../HarpFace'
 import { ControlPanel } from './index'
 
 
-test('ControlPanel has a first Pozition control which calls it\'s parameterised function when clicked', () => {
+test('ControlPanel has a first Pozition control which calls it\'s parameterised function with it\'s expected identity when clicked', () => {
   const setApparatusId = jest.fn()
   const setPozitionId = jest.fn()
   const setPitchId = jest.fn()
@@ -17,11 +17,14 @@ test('ControlPanel has a first Pozition control which calls it\'s parameterised 
   const { getByText } = render(<ControlPanel {...controlPanelProps} />)
 
   expect(getByText(PozitionIds.First)).toBeInTheDocument()
+
   fireEvent.click(getByText(PozitionIds.First))
+
   expect(setPozitionId.mock.calls.length).toBe(1)
+  expect(setPozitionId.mock.calls[0][0]).toBe(PozitionIds.First)
 })
 
-test('ControlPanel has a Major Diatonic Apparatus control which calls it\'s parameterised function when clicked', () => {
+test('ControlPanel has a Major Diatonic Apparatus control which calls it\'s parameterised function with it\'s expected identity when clicked', () => {
   const setApparatusId = jest.fn()
   const setPozitionId = jest.fn()
   const setPitchId = jest.fn()
@@ -30,11 +33,14 @@ test('ControlPanel has a Major Diatonic Apparatus control which calls it\'s para
   const { getByText } = render(<ControlPanel {...controlPanelProps} />)
 
   expect(getByText(ApparatusIds.MajorDiatonic)).toBeInTheDocument()
+
   fireEvent.click(getByText(ApparatusIds.MajorDiatonic))
+
   expect(setApparatusId.mock.calls.length).toBe(1)
+  expect(setApparatusId.mock.calls[0][0]).toBe(ApparatusIds.MajorDiatonic)
 })
 
-test('ControlPanel has an C Pitch control which calls it\'s parameterised function when clicked', () => {
+test('ControlPanel has an C Pitch control which calls it\'s parameterised function with it\'s expected identity when clicked', () => {
   const setApparatusId = jest.fn()
   const setPozitionId = jest.fn()
   const setPitchId = jest.fn()
@@ -43,11 +49,14 @@ test('ControlPanel has an C Pitch control which calls it\'s parameterised functi
   const { getByText } = render(<ControlPanel {...controlPanelProps} />)
 
   expect(getByText(PitchIds.C)).toBeInTheDocument()
+
   fireEvent.click(getByText(PitchIds.C))
+
   expect(setPitchId.mock.calls.length).toBe(1)
+  expect(setPitchId.mock.calls[0][0]).toBe(PitchIds.C)
 })
 
-test('ControlPanel has a DisplayModeToggler control which calls it\'s parameterised function when clicked', () => {
+test('ControlPanel has a DisplayModeToggler control which calls it\'s parameterised function with it\'s expected identity when clicked', () => {
   const setApparatusId = jest.fn()
   const setPozitionId = jest.fn()
   const setPitchId = jest.fn()
@@ -56,6 +65,9 @@ test('ControlPanel has a DisplayModeToggler control which calls it\'s parameteri
   const { getByText } = render(<ControlPanel {...controlPanelProps} />)
 
   expect(getByText(DisplayModes.Degree)).toBeInTheDocument()
+
   fireEvent.click(getByText(DisplayModes.Degree))
+
   expect(setDisplayMode.mock.calls.length).toBe(1)
+  expect(setDisplayMode.mock.calls[0][0]).toBe(DisplayModes.Degree)
 })
