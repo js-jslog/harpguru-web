ce00d90ee206c459795629d73419c20b516942cb
js-jslog <jhs4jbs@hotmail.co.uk>
2020-05-14 18:01:01 +0000

    Fill out all of the control buttons
    This is sub-optimal, but as mentioned before, the final control panel
    probably isn't going to be using buttons like this at all. I just need
    something which works for now.

--- src/ControlPanel/PitchButtons/PitchButtons/components/index.tsx
+++ src/ControlPanel/PitchButtons/PitchButtons/components/index.tsx
@@ -7,16 +7,36 @@ import { PitchButton } from '../../PitchButton'
 import type { PitchButtonProps } from '../../PitchButton'
 
 export function PitchButtons(props: PitchButtonsProps): ReactElement {
-  const { C, Db } = PitchIds
+  const { A, Bb, B, C, Db, D, Eb, E, F, Gb, G, Ab } = PitchIds
   const { setPitchId } = props
+  const pitchAPitchButtonProps: PitchButtonProps = { id: A, setPitchId }
+  const pitchBbPitchButtonProps: PitchButtonProps = { id: Bb, setPitchId }
+  const pitchBPitchButtonProps: PitchButtonProps = { id: B, setPitchId }
   const pitchCPitchButtonProps: PitchButtonProps = { id: C, setPitchId }
   const pitchDbPitchButtonProps: PitchButtonProps = { id: Db, setPitchId }
+  const pitchDPitchButtonProps: PitchButtonProps = { id: D, setPitchId }
+  const pitchEbPitchButtonProps: PitchButtonProps = { id: Eb, setPitchId }
+  const pitchEPitchButtonProps: PitchButtonProps = { id: E, setPitchId }
+  const pitchFPitchButtonProps: PitchButtonProps = { id: F, setPitchId }
+  const pitchGbPitchButtonProps: PitchButtonProps = { id: Gb, setPitchId }
+  const pitchGPitchButtonProps: PitchButtonProps = { id: G, setPitchId }
+  const pitchAbPitchButtonProps: PitchButtonProps = { id: Ab, setPitchId }
 
   return (
     <div role='menu'>
       <div>Harp Key Pitch</div>
+      <PitchButton {...pitchAPitchButtonProps} />
+      <PitchButton {...pitchBbPitchButtonProps} />
+      <PitchButton {...pitchBPitchButtonProps} />
       <PitchButton {...pitchCPitchButtonProps} />
       <PitchButton {...pitchDbPitchButtonProps} />
+      <PitchButton {...pitchDPitchButtonProps} />
+      <PitchButton {...pitchEbPitchButtonProps} />
+      <PitchButton {...pitchEPitchButtonProps} />
+      <PitchButton {...pitchFPitchButtonProps} />
+      <PitchButton {...pitchGbPitchButtonProps} />
+      <PitchButton {...pitchGPitchButtonProps} />
+      <PitchButton {...pitchAbPitchButtonProps} />
     </div>
   )
 }
